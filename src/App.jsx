9c4848f77d1ea715222e36e1a1e5132c01bd748f
kasra torabi <Kasra.torabiii@gmail.com>
2025-02-03 21:42:08 +0330

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Highlights from "./components/Highlights"
+
+const App = () => {
+
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
